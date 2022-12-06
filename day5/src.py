502ae5d76c4b8f90689a73e8ff7b0d0938492a3e
Jakob Karlstrand <jakobkarlstrand@gmail.com>
2022-12-06 08:10:42 +0100

    day 6

--- day5/src.py
+++ day5/src.py
@@ -1,60 +1,43 @@
 def read_input(filepath):
     file = open(filepath, 'r')
-    return file.readlines()
+    return file.read()
 
-
-lines = read_input("day4/input.txt")
-crates = {
-    "1": ["D","M","S","Z","R","F","W","N"],
-    "2": ["W","P","Q", "G", "S"],
-    "3": ["W", "R","V", "Q", "F", "N", "J", "C"],
-    "4": ["F", "Z", "P", "C", "G", "D", "L"],
-    "5": ["T", "P", "S"],
-    "6": ["H", "D", "F","W", "R", "L"],
-    "7": ["Z", "N", "D", "C"],
-    "8": ["W", "N", "R", "F", "V", "S", "J", "Q"],
-    "9": ["R", "M", "S", "G", "Z", "W", "V"],
-}
 
 def part1():
-    # MADE THe INPUT FORMAT FROM 'move X from a to b' INTO --> X-a:b
-    for line in lines:
-        splitted = line.split("-")
-        n = splitted[0]
+    sequence = read_input("day5/input.txt")
+    length_message = 3
+    idx = 3
 
-        [_from, _to] = splitted[1].split(":")
-        _to = _to.replace("\n", "")
-        for i in range(int(n)):
-            crates[_to].append(crates[_from].pop())
-        
+    while idx < len(sequence):
+        arr = list(sequence[idx-length_message:idx+1])
+   
 
+        s = set(arr)
 
-
-    answer = ""
-    for s in crates:
-        answer += (crates[s][-1])
-    print("PART1: ",answer)
+        if len(s) == len(arr): #Unique
+            print("ANSWER:",idx+1)
+            break
+        else:
+            idx+=1
 
 
+    
 def part2():
-    # MADE THe INPUT FORMAT FROM 'move X from a to b' INTO --> X-a:b
-    for line in lines:
-        splitted = line.split("-")
-        n = splitted[0]
+    sequence = read_input("day5/input.txt")
+    length_message = 13
+    idx = 13
 
-        [_from, _to] = splitted[1].split(":")
+    while idx < len(sequence):
+        arr = list(sequence[idx-length_message:idx+1])
+    
 
-        temp_arr = []
-        for i in range(int(n)):
-            temp_arr.append(crates[_from].pop())
-        
-        crates[_to.replace("\n", "")] += reversed(temp_arr)
+        s = set(arr)
 
-
-    answer = ""
-    for s in crates:
-        answer += (crates[s][-1])
-    print("PART2: ",answer)
-
+        if len(s) == len(arr): #Unique
+            print("ANSWER:",idx+1)
+            break
+        else:
+            idx+=1
 
 part1()
+part2()
