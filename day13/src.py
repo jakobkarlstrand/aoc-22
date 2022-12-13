95e9ea63bcc57c5d1b882953c1d424dc5d68d3e6
Jakob Karlstrand <jakobkarlstrand@gmail.com>
2022-12-13 15:12:08 +0100

    prettier code

--- day13/src.py
+++ day13/src.py
@@ -17,51 +17,34 @@ def parse_input():
     for pair in lines:
        
         left,right = pair.split("\n")
-        left_list = eval(left)
-        right_list = eval(right)
-        lists.append((left_list,right_list))
+        lists.append((eval(left),eval(right)))
     return lists
 
 
-# [[1],[2,3,4]]
-# [[1],4]
 
 
 def compare_lists(l1,l2):
-    if type(l1) != list and type(l2) != list:
-        if l1 > l2:
-            return -1
-        elif l2 > l1:
-            return 1
-        else:
-            return 0
+    if not list in [type(l1), type(l2)]:
+        d = (l2 - l1)
+        return d/abs(d) if d != 0 else 0
+    
 
+    
     if type(l1) != list:
         l1 = [l1]
     if type(l2) != list:
-        l2 = [l2]
+        l2 =[l2]
     _idx = 0
 
 
 
     while _idx < len(l1) and _idx < len(l2):
 
-        if type(l1[_idx]) == list or type(l2[_idx]) == list:
-            cmp = compare_lists(l1[_idx],l2[_idx])
-            if cmp == -1:
-                return -1
-            elif cmp == 1:
-                return 1
-            else:
-                _idx+=1
-                continue
- 
-        if abs(l1[_idx] - l2[_idx]) != 0:
-
-            return 1 if l1[_idx] < l2[_idx] else  -1
-        
-        
+        cmp = compare_lists(l1[_idx],l2[_idx])
+        if cmp != 0:
+            return cmp
         _idx+=1
+
     
     if len(l1) < len(l2): return 1
     if len(l1) > len(l2): return -1 
@@ -78,10 +61,8 @@ def p1():
     n_sorted = 0
     idx = 1
 
-    for pair in input:
-
-    
-        compare = compare_lists(pair[0],pair[1])
+    for left,right in input:
+        compare = compare_lists(left,right)
         
         if compare == 1:
             n_sorted+=idx
@@ -93,29 +74,26 @@ def p1():
 
 def p2():
     input = parse_input()
-    divider_packet1 = [[2]]
-    divider_packet2 = [[6]]
+    divider_packets = [[[2]],[[6]]]
+
     all_packets = []
-    for left,right in input:
-        all_packets.append(left)
-        all_packets.append(right)
-    all_packets.append(divider_packet1)
-    all_packets.append(divider_packet2)
-    
-    idx = 1
+    for pair in input:
+        all_packets += pair
+
+    all_packets += divider_packets
+
 
     for i in range(len(all_packets)-1):
         for j in range(i,len(all_packets)):
             if compare_lists(all_packets[i], all_packets[j]) == -1:
-                temp = all_packets[j]
-                all_packets[j] = all_packets[i]
-                all_packets[i] = temp
+                [all_packets[i],all_packets[j]] = [all_packets[j], all_packets[i]]
+    
     signal = 1
-    idx = 1
-    for packet in all_packets:
-        if packet == divider_packet1 or packet == divider_packet2:
-            signal *= idx
-        idx += 1
+    
+    for i,packet in enumerate(all_packets):
+        if packet in divider_packets:
+            signal *= i
+    
 
     print("PART 2: ", signal)
     
